57d12b8a61a81b24974a8bf699c267fcba9e8879
tb1015 <57799359+tb1015@users.noreply.github.com>
2022-10-23 13:19:11 +0700

    Add files via upload

--- global_warming_economy.py
+++ global_warming_economy.py
@@ -1,25 +1,28 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import plotly.express as px
+import statsmodels
 import seaborn as sb
 import matplotlib.pyplot as plt
 
 
-#pilihan tahun baiknya dropdown list
 #kasih subtopik di tiap paragraf di bagian bawah
 #GDP Dunia?
 
-#Link Streamlit diperbaiki
 #Plotly untuk tampilan data yang interaktif
 #Gunakan juga space disamping
 
 
 ##==================================================================================================================================
-#st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 st.title("Global Warming in Economic Perspective")
 st.header("Introduction")
-st.write("Economic development fueled by fossil energy increases Greenhouse Gasses (GHGs) accumulated in our atmosphere. \
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.write("Economic development fueled by fossil energy increases Greenhouse Gasses (GHGs) accumulated in our atmosphere. \
         Such accumulation intensifies the Greenhouse effect in atmosphere leading to increasing global temperature (as shown by the figure of global temperature anomaly bellow).\
         This increasing global temperature will lead to the well known climate change problem.")
 
@@ -66,28 +69,24 @@ plt.grid()
 plt.text(1950, 4.5, 'Data sources: climate.nasa.gov, Global Carbon Project (2021)')
 
 
-#sb.set(font_scale=1.0, style="ticks") #set styling preferences
-#points = plt.scatter(df_gtemp["Lowess(5)"], df_gtemp["Year"],
-#                    c=df_gtemp["No_Smoothing"], s=100, cmap="Spectral") #set style options
-
-#add a color bar
-#plt.colorbar(points)
-
-#set limits
-#plt.xlim(min(df_gtemp["Year"]), max(df_gtemp["Year"]))
-#plt.ylim(min(df_gtemp["No_Smoothing"]), max(df_gtemp["No_Smoothing"]))
-
-#build the plot
-#Fig = sb.regplot(x="Year",y="Lowess(5)", data=df_gtemp, scatter=True, color="grey", logx=True)#,x_estimator=np.mean)
-#Fig = Fig.set(ylabel='Temperature Anomaly (°C)', xlabel='Year') #add labels
-#plot = sb.lmplot(x="Year", y="Lowess(5)", data=df_gtemp)
-#plt.grid()
-st.pyplot(plt.gcf())
-
+with col2:
+    #st.pyplot(plt.gcf())
+    st.write("**Annual CO2 Emissions (in billion tCO2) and Annomaly Temperature (in °C)**")
+    st.write("Data sources: climate.nasa.gov, Global Carbon Project (2021)")
+    fig = px.scatter(data, x=x_axis, y=y_axis, color=c_axis, trendline="ols",
+                     labels=dict(Temperature="Annomaly in °C"))
+   
+    fig.update_traces(marker=dict(size=8,
+                              line=dict(width=1,
+                                        color='White')),
+                  selector=dict(mode='markers'))
+    
+    st.plotly_chart(fig, use_container_width=True)
+    
+    
 ##==================================================================================================================================
 ## Change Global GDP vs CO2 emission per capita
 st.header("Change Global GDP vs CO2 emission")
-
 st.write("Significant historical emission has been released to our atmosphere to achieve today advance economy.\
         This can be seen statistically from the following graph showing annual CO2 emissions per capita and the corresponding GDP per capita. \
         Most of the GDP development in developing countries are fueled by fossil fuels. Thus, the global CO2 emission is likely continuing to rise. \
@@ -95,66 +94,23 @@ st.write("Significant historical emission has been released to our atmosphere to
         Carbon pricing alone without energy transition will only slow or retard the economy development.\
         Such solution is hardly realized in the developing countries. ")
 
-# df_gdp_gr = pd.read_csv("Data/gdp-per-capita-growth.csv")
-# df_co2p = pd.read_csv("Data/co-emissions-per-capita.csv")
-# y0 = 1961
-# y1 = 2020
-# country = "Indonesia"
-# df_Ggdp_gr = df_gdp_gr[df_gdp_gr["Entity"]=="World"].reset_index(drop=True)
-# df_Gco2p = df_co2p[df_co2p["Entity"]=="World"].reset_index(drop=True)
-
-# #Combining the GDP data, the emission data, and the temperature data  with the same year
-# data = df_Ggdp_gr[df_Ggdp_gr["Year"].between(y0,y1, inclusive=True)].reset_index(drop=True)
-# data["CO2 emission per capita (tCO2e)"] = df_Gco2p[df_Gco2p["Year"].between(y0, y1, inclusive=True)]["Annual CO2 emissions (per capita)"].reset_index(drop=True) # Combine the emission and the temperature data
-# data["Temperature (C)"] = df_gtemp[df_gtemp["Year"].between(y0, y1, inclusive=True)]["Lowess(5)"].reset_index(drop=True) # Combine the emission and the temperature data
-
-# y_axis = "CO2 emission per capita (tCO2e)"
-# c_axis = "GDP per capita growth (annual %)"
-# x_axis = "Year"#"Temperature (C)"
-
-# sb.set(font_scale=1.0, style="ticks") #set styling preferences
-# points1 = plt.scatter(data[x_axis], data[y_axis], c=data[c_axis], s=100) #set style options
-
-# #add a color bar
-# cbar1 = plt.colorbar(points1)
-# cbar1.set_label(c_axis)
-
-# #set limits
-# plt.xlim(min(data[x_axis]), max(data[x_axis]))
-# plt.ylim(min(data[y_axis]), max(data[y_axis]))
-
-# #build the plot
-# plot = sb.regplot(x=x_axis,y=y_axis, data=data, scatter=True, color="black")#, logx=True)#,x_estimator=np.mean)
-# plot = plot.set(ylabel=y_axis, xlabel=x_axis) #add labels
-# plt.grid()  
-# plt.text(1950, 2.7, 'Data source: Global Carbon Project (2021)')
-# st.pyplot(plt.gcf())
-
 #url2='https://drive.google.com/file/d/1xMT64NajKlGgDdOWlI8mnA0ljUYZkmqr/view?usp=sharing'
 #url2='https://drive.google.com/uc?id=' + url2.split('/')[-2]
 #url2 = "Data/co2-emissions-and-gdp-long-term.csv"
 url2 = "https://github.com/tb1015/Capstone/blob/main/co2-emissions-and-gdp-long-term.csv?raw=true"
 df_gdp_co2 = pd.read_csv(url2)
-#df_gdp_co2 = pd.read_csv("Data/co2-emissions-and-gdp-long-term.csv")
-#year_range = st.slider("Year range of emission & GDP data", value=[1900,2020])
 
-st.write("**Please select the time range and the region for the following chart.**")
 col1, col2 = st.columns(2)
 
 with col1:
-    y0b = st.selectbox("Initial year",(np.linspace(1900,1960,7)).astype(int))
-    st.write('You selected:', y0b)
-    
-with col2:
-    y1b = st.selectbox("Final year",np.linspace(1970,2020,6).astype(int))
-    st.write('You selected:', y1b)
-
-#y0b = st.select_slider("Select Initial Year",(np.linspace(1900,1960,7)).astype(int))
-#y1b = st.select_slider("Select End Year",np.linspace(1970,2020,6).astype(int))
-
-#country = st.text_input("Enter Country/Region")
-country = st.selectbox("Enter Country/Region",df_gdp_co2["Entity"].unique(),index=103)
-#st.write('You selected:', country)
+    st.write("**Please select the time range and the region for the emission & GDP chart.**")
+    y0b = st.selectbox("Initial year",(np.linspace(1900,1960,7)).astype(int), index=5)
+    #st.write('You selected:', y0b)
+    y1b = st.selectbox("Final year",np.linspace(1970,2020,6).astype(int), index=5)
+    #st.write('You selected:', y1b)
+    #country = st.text_input("Enter Country/Region")
+    country = st.selectbox("Enter Country/Region",df_gdp_co2["Entity"].unique(),index=103)
+    #st.write('You selected:', country)
 
 df_Ggdp_co2 = df_gdp_co2[df_gdp_co2["Entity"]==country].reset_index(drop=True)
 
@@ -177,14 +133,19 @@ ax.set(ylabel=y_axis+" in tCO2e")
 ax2.set(ylabel=c_axis+" in US$")
 plt.grid()  #just add this
 #plt.show()
-st.pyplot(plt.gcf())
+
+with col2:
+    st.pyplot(plt.gcf())
 
 
 ##=================================================================================================================
 ## Global CO2 emissions from fossil fuels and land use change
 st.header("Global CO2 emissions from fossil fuels and land use change")
 
-st.write("Most of our emission comes from fossil fuels, either for fueling our industry or our transportation system.")
+col1,col2 = st.columns(2)
+
+with col1:
+    st.write("Most of our emission comes from fossil fuels, either for fueling our industry or our transportation system.")
 
 url3='https://drive.google.com/file/d/1iYmn0vxSuZJZ2gN0X1Ub5e6WV37GmNZu/view?usp=sharing'
 url3='https://drive.google.com/uc?id=' + url3.split('/')[-2]
@@ -216,8 +177,9 @@ plt.title(countryc, loc="left", size=20)
 ax.set(ylabel=y_axis+" in billion tCO2")
 ax2.set(ylabel=c_axis+" in billion tCO2")
 plt.grid()  #just add this
-st.pyplot(plt.gcf())
 
+with col2:
+    st.pyplot(plt.gcf())
 
 st.header("Carbon Pricing and Energy Transition")
 
@@ -231,6 +193,8 @@ st.write("Clean tech R&D and industrialization are beyond developing countries e
 
 st.write("Access to affordable clean technologies and resources required to manufacture the technology is necessary to mitigate the GHG emission potential while also allowing developing countries to grow their economy cleanly.")
 st.write("How to obtain the affordable clean technology and manufacture? Sharing resources to grow businesses producing the technology and infrastructure as close as possible to the implementation site.")
+
+
 
 #st.subheader("Ini subheader")
 #st.metric("Sales", 100, "-4%")
